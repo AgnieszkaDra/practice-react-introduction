263c8fa8e28e49c65ae3974413e1b70788a5973f
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-02-28 21:02:37 +0100

    end task 02

---
+++ 02/app.js
@@ -0,0 +1,30 @@
+import React from'react';
+import { createRoot } from'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'));
+const myStyles = {
+    border:'1px solid blue',
+    backgroundColor:'yellow',
+};
+
+const HeaderFn = () => {
+    return (
+        <header style={myStyles}>
+            'Moja pierwsza strona w React'
+        </header>
+    )
+}
+
+class HeaderCl extends React.Component {
+    render() {
+        return (
+        <header>
+            'Moja pierwsza strona w React'
+        </header>
+        )
+    }
+}
+
+root.render(
+    <><HeaderFn /><HeaderCl /></>
+    );s
